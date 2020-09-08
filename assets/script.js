932f1ba93dd22b73a198a2a59576fa4f657ebcf9
esgowens <kinggsone@gmail.com>
2020-09-08 14:27:02 -0400

    adjusted tileSize

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,24 @@ function setup () {
 function draw () {
     background("#000000")
 
+    const tileSize = 50
 
-    image(graphic, 0, 0, 1200, 100, 0, 0, 1200, 600)
+    for (let y = 0; y < 12; y = y +1) {
+
+        const sx = 0
+        const sy = y * tileSize
+        const sw = 1200
+        const sh = tileSize
+        
+        const dx = 0
+        const dy = y * tileSize
+        const dw = 1200
+        const dh = tileSize
+
+
+        image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
+    }
+
+
+    
 }
